0a1aeadaa28e7b73fc682029fe24e62d094e1e87
CarlosZoft <carlosk2gunn@hotmail.com>
2023-01-22 21:29:38 -0300

    ✨ feat: create router

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
 import "dotenv/config";
+import express, { Request, Response } from "express";
 import { syncFollowing } from "@/controller/syncFollowing";
 
-const main = async () => {
-  await syncFollowing("CarlosZoft");
-};
+const app = express();
 
-main().catch((e) => console.error(e));
+app.post(
+  "/syncFollowers/:login",
+  async (request: Request, response: Response) => {
+    try {
+      const { login } = request.params;
+      await syncFollowing(login);
+
+      response.send("Success");
+    } catch (error) {
+      response.status(500).send("Unexpected error")
+    }
+  }
+);
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
